24fd8f676f35cff4e83f939c1f9c7f7b22d8a304
NaufalHC <35517774+NaufalHC@users.noreply.github.com>
2019-03-21 10:32:35 +0700

    Add files via upload

---
+++ Klient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class Client
+{
+    const int PORT_NO = 5000;
+    const string SERVER_IP = "127.0.0.1";
+
+    public static void Start()
+    {
+        Console.Clear();
+        Console.WriteLine("Client");
+
+        Data player = new Data();
+        player.Id.player = "crowisover2";
+        player.Id.ModelCharacter = "grunt1";
+        player.Id.Senjata = "AK-56";
+        player.XYplayer.X = 1;
+        player.XYplayer.Y = 2;
+        player.XYbullet.X = 1;
+        player.XYbullet.Y = 2;
+        player.Status_dead_alive = "dead";
+
+        //---create a TCPClient object at the IP and port no.---
+        TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+        NetworkStream nwStream = client.GetStream();
+        byte[] bytesToSend = byteData.serializable(player);
+
+        //---send the text---
+        nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+        int dataLength = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+        //---convert the data received into a string---
+        Data dataEnemy = (Data)byteData.Deserializable(buffer, dataLength);
+        if (dataLength == null) Console.WriteLine("NULL");
+        dataEnemy.Show();
+
+        //---read back the text---
+        /*            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                    int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                    Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));*/
+        Console.ReadLine();
+        client.Close();
+    }
+}
---
+++ Serper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class Server
+{
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "127.0.0.1";
+
+        public static void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("Server");
+
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---nmb,jughf
+
+            //---read incoming stream---
+            using (NetworkStream nwStream = client.GetStream())
+            {
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                int dataLength = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //---convert the data received into a string---
+                Data dataEnemy = (Data)byteData.Deserializable(buffer, dataLength);
+                if (dataLength == null) Console.WriteLine("NULL");
+                dataEnemy.Show();
+
+                byte[] bytesToSend = byteData.serializable(dataEnemy);
+
+                //---send the text---
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            }
+            //---write back the text to the client---
+
+            // nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
